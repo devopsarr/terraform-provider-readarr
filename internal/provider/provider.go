c2a76819456fbd7e6a929a6edfc2d0f5561c8ebe
Fuochi <ffuochi@hotmail.com>
2022-12-14 14:19:40 +0400

    build(deps): bump github.com/hashicorp/terraform-plugin-framework-validators

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -18,7 +18,6 @@ import (
 
 // Ensure provider defined types fully satisfy framework interfaces.
 var _ provider.Provider = &ReadarrProvider{}
-var _ provider.ProviderWithMetadata = &ReadarrProvider{}
 
 // ScaffoldingProvider defines the provider implementation.
 type ReadarrProvider struct {
