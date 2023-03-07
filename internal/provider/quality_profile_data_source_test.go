8da976bac64b5ab0756b8b0d7d81793750700305
Fuochi <ffuochi@hotmail.com>
2023-03-07 14:39:38 +0400

    feat: add quality profile data source

---
+++ internal/provider/quality_profile_data_source_test.go
@@ -0,0 +1,45 @@
+package provider
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccQualityProfileDataSource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Unauthorized
+			{
+				Config:      testAccQualityProfileDataSourceConfig("Error") + testUnauthorizedProvider,
+				ExpectError: regexp.MustCompile("Client Error"),
+			},
+			// Not found testing
+			{
+				Config:      testAccQualityProfileDataSourceConfig("Error"),
+				ExpectError: regexp.MustCompile("Unable to find quality_profile"),
+			},
+			// Read testing
+			{
+				Config: testAccQualityProfileDataSourceConfig("eBook"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.readarr_quality_profile.test", "id"),
+					resource.TestCheckResourceAttr("data.readarr_quality_profile.test", "cutoff", "2")),
+			},
+		},
+	})
+}
+
+func testAccQualityProfileDataSourceConfig(name string) string {
+	return fmt.Sprintf(`
+	data "readarr_quality_profile" "test" {
+		name = "%s"
+	}
+	`, name)
+}
