d7f92e58f54eecc0593e09160984fb552a24237a
Fuochi <ffuochi@hotmail.com>
2023-03-07 15:43:25 +0400

    feat: add indexer filelist resource

---
+++ internal/provider/indexer_filelist_resource.go
@@ -0,0 +1,315 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/devopsarr/readarr-go/readarr"
+	"github.com/devopsarr/terraform-provider-readarr/internal/helpers"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+const (
+	indexerFilelistResourceName   = "indexer_filelist"
+	indexerFilelistImplementation = "FileList"
+	indexerFilelistConfigContract = "FileListSettings"
+	indexerFilelistProtocol       = "torrent"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var (
+	_ resource.Resource                = &IndexerFilelistResource{}
+	_ resource.ResourceWithImportState = &IndexerFilelistResource{}
+)
+
+func NewIndexerFilelistResource() resource.Resource {
+	return &IndexerFilelistResource{}
+}
+
+// IndexerFilelistResource defines the Filelist indexer implementation.
+type IndexerFilelistResource struct {
+	client *readarr.APIClient
+}
+
+// IndexerFilelist describes the Filelist indexer data model.
+type IndexerFilelist struct {
+	Tags                    types.Set     `tfsdk:"tags"`
+	Categories              types.Set     `tfsdk:"categories"`
+	Name                    types.String  `tfsdk:"name"`
+	BaseURL                 types.String  `tfsdk:"base_url"`
+	Username                types.String  `tfsdk:"username"`
+	Passkey                 types.String  `tfsdk:"passkey"`
+	Priority                types.Int64   `tfsdk:"priority"`
+	ID                      types.Int64   `tfsdk:"id"`
+	MinimumSeeders          types.Int64   `tfsdk:"minimum_seeders"`
+	EarlyReleaseLimit       types.Int64   `tfsdk:"early_release_limit"`
+	SeedTime                types.Int64   `tfsdk:"seed_time"`
+	DiscographySeedTime     types.Int64   `tfsdk:"author_seed_time"`
+	SeedRatio               types.Float64 `tfsdk:"seed_ratio"`
+	EnableAutomaticSearch   types.Bool    `tfsdk:"enable_automatic_search"`
+	EnableRss               types.Bool    `tfsdk:"enable_rss"`
+	EnableInteractiveSearch types.Bool    `tfsdk:"enable_interactive_search"`
+}
+
+func (i IndexerFilelist) toIndexer() *Indexer {
+	return &Indexer{
+		EnableAutomaticSearch:   i.EnableAutomaticSearch,
+		EnableInteractiveSearch: i.EnableInteractiveSearch,
+		EnableRss:               i.EnableRss,
+		Priority:                i.Priority,
+		ID:                      i.ID,
+		Name:                    i.Name,
+		MinimumSeeders:          i.MinimumSeeders,
+		EarlyReleaseLimit:       i.EarlyReleaseLimit,
+		SeedTime:                i.SeedTime,
+		DiscographySeedTime:     i.DiscographySeedTime,
+		SeedRatio:               i.SeedRatio,
+		Username:                i.Username,
+		Passkey:                 i.Passkey,
+		BaseURL:                 i.BaseURL,
+		Tags:                    i.Tags,
+		Categories:              i.Categories,
+		Implementation:          types.StringValue(indexerFilelistImplementation),
+		ConfigContract:          types.StringValue(indexerFilelistConfigContract),
+		Protocol:                types.StringValue(indexerFilelistProtocol),
+	}
+}
+
+func (i *IndexerFilelist) fromIndexer(indexer *Indexer) {
+	i.EnableAutomaticSearch = indexer.EnableAutomaticSearch
+	i.EnableInteractiveSearch = indexer.EnableInteractiveSearch
+	i.EnableRss = indexer.EnableRss
+	i.Priority = indexer.Priority
+	i.ID = indexer.ID
+	i.Name = indexer.Name
+	i.MinimumSeeders = indexer.MinimumSeeders
+	i.EarlyReleaseLimit = indexer.EarlyReleaseLimit
+	i.SeedTime = indexer.SeedTime
+	i.DiscographySeedTime = indexer.DiscographySeedTime
+	i.SeedRatio = indexer.SeedRatio
+	i.Username = indexer.Username
+	i.Passkey = indexer.Passkey
+	i.BaseURL = indexer.BaseURL
+	i.Tags = indexer.Tags
+	i.Categories = indexer.Categories
+}
+
+func (r *IndexerFilelistResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_" + indexerFilelistResourceName
+}
+
+func (r *IndexerFilelistResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "<!-- subcategory:Indexers -->Indexer FileList resource.\nFor more information refer to [Indexer](https://wiki.servarr.com/readarr/settings#indexers) and [FileList](https://wiki.servarr.com/readarr/supported#filelist).",
+		Attributes: map[string]schema.Attribute{
+			"enable_automatic_search": schema.BoolAttribute{
+				MarkdownDescription: "Enable automatic search flag.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"enable_interactive_search": schema.BoolAttribute{
+				MarkdownDescription: "Enable interactive search flag.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"enable_rss": schema.BoolAttribute{
+				MarkdownDescription: "Enable RSS flag.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"priority": schema.Int64Attribute{
+				MarkdownDescription: "Priority.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"name": schema.StringAttribute{
+				MarkdownDescription: "IndexerFilelist name.",
+				Required:            true,
+			},
+			"tags": schema.SetAttribute{
+				MarkdownDescription: "List of associated tags.",
+				Optional:            true,
+				Computed:            true,
+				ElementType:         types.Int64Type,
+			},
+			"id": schema.Int64Attribute{
+				MarkdownDescription: "IndexerFilelist ID.",
+				Computed:            true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.UseStateForUnknown(),
+				},
+			},
+			// Field values
+			"minimum_seeders": schema.Int64Attribute{
+				MarkdownDescription: "Minimum seeders.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"early_release_limit": schema.Int64Attribute{
+				MarkdownDescription: "Early release limit.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"seed_time": schema.Int64Attribute{
+				MarkdownDescription: "Seed time.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"author_seed_time": schema.Int64Attribute{
+				MarkdownDescription: "Author seed time.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"seed_ratio": schema.Float64Attribute{
+				MarkdownDescription: "Seed ratio.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"base_url": schema.StringAttribute{
+				MarkdownDescription: "Base URL.",
+				Optional:            true,
+				Computed:            true,
+			},
+			"passkey": schema.StringAttribute{
+				MarkdownDescription: "Passkey.",
+				Required:            true,
+				Sensitive:           true,
+			},
+			"username": schema.StringAttribute{
+				MarkdownDescription: "Username.",
+				Required:            true,
+			},
+			"categories": schema.SetAttribute{
+				MarkdownDescription: "Categories list.",
+				Optional:            true,
+				Computed:            true,
+				ElementType:         types.Int64Type,
+			},
+		},
+	}
+}
+
+func (r *IndexerFilelistResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if client := helpers.ResourceConfigure(ctx, req, resp); client != nil {
+		r.client = client
+	}
+}
+
+func (r *IndexerFilelistResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	// Retrieve values from plan
+	var indexer *IndexerFilelist
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &indexer)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Create new IndexerFilelist
+	request := indexer.read(ctx)
+
+	response, _, err := r.client.IndexerApi.CreateIndexer(ctx).IndexerResource(*request).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError(helpers.ClientError, helpers.ParseClientError(helpers.Create, indexerFilelistResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "created "+indexerFilelistResourceName+": "+strconv.Itoa(int(response.GetId())))
+	// Generate resource state struct
+	indexer.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &indexer)...)
+}
+
+func (r *IndexerFilelistResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	// Get current state
+	var indexer *IndexerFilelist
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &indexer)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Get IndexerFilelist current value
+	response, _, err := r.client.IndexerApi.GetIndexerById(ctx, int32(indexer.ID.ValueInt64())).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError(helpers.ClientError, helpers.ParseClientError(helpers.Read, indexerFilelistResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "read "+indexerFilelistResourceName+": "+strconv.Itoa(int(response.GetId())))
+	// Map response body to resource schema attribute
+	indexer.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &indexer)...)
+}
+
+func (r *IndexerFilelistResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	// Get plan values
+	var indexer *IndexerFilelist
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &indexer)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Update IndexerFilelist
+	request := indexer.read(ctx)
+
+	response, _, err := r.client.IndexerApi.UpdateIndexer(ctx, strconv.Itoa(int(request.GetId()))).IndexerResource(*request).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError(helpers.ClientError, fmt.Sprintf("Unable to update "+indexerFilelistResourceName+", got error: %s", err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "updated "+indexerFilelistResourceName+": "+strconv.Itoa(int(response.GetId())))
+	// Generate resource state struct
+	indexer.write(ctx, response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &indexer)...)
+}
+
+func (r *IndexerFilelistResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var indexer *IndexerFilelist
+
+	resp.Diagnostics.Append(req.State.Get(ctx, &indexer)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Delete IndexerFilelist current value
+	_, err := r.client.IndexerApi.DeleteIndexer(ctx, int32(indexer.ID.ValueInt64())).Execute()
+	if err != nil {
+		resp.Diagnostics.AddError(helpers.ClientError, helpers.ParseClientError(helpers.Read, indexerFilelistResourceName, err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "deleted "+indexerFilelistResourceName+": "+strconv.Itoa(int(indexer.ID.ValueInt64())))
+	resp.State.RemoveResource(ctx)
+}
+
+func (r *IndexerFilelistResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	helpers.ImportStatePassthroughIntID(ctx, path.Root("id"), req, resp)
+	tflog.Trace(ctx, "imported "+indexerFilelistResourceName+": "+req.ID)
+}
+
+func (i *IndexerFilelist) write(ctx context.Context, indexer *readarr.IndexerResource) {
+	genericIndexer := i.toIndexer()
+	genericIndexer.write(ctx, indexer)
+	i.fromIndexer(genericIndexer)
+}
+
+func (i *IndexerFilelist) read(ctx context.Context) *readarr.IndexerResource {
+	return i.toIndexer().read(ctx)
+}
